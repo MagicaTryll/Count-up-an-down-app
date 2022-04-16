36020831bef742c8ef110e8db7aedecfe2911383
MagicaTryll <49566876+MagicaTryll@users.noreply.github.com>
2022-04-16 10:47:33 +0200

    Add files via upload

---
+++ round_buttons.js
@@ -0,0 +1,19 @@
+let countEl = document.getElementById("count-el")
+let count = 1
+    //get random value from array
+const colors = ["red", "blue", "green", "black", "brown", "hotpink", "pink", "silver", "purple"];
+const randColor = colors[Math.floor(Math.random() * colors.length)];
+
+function increment() {
+    count += 1;
+    countEl.textContent = count
+    if (count % 10 === 0) {
+        document.getElementById("color-el").style.background = randColor;
+    }
+}
+
+
+function decrement() {
+    count -= 1;
+    countEl.textContent = count
+}
